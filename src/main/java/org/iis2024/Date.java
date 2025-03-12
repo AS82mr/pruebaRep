24c14d5f7d45cae3f047ed31d7466c1cfbafce31
ShahryarMirzaei <shmir1384@uma.es>
2025-03-12 14:16:42 +0100

    added new methods and classes exe7 part1

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -88,4 +88,14 @@ private int AddednewvarByTheContributor ;
       return false;
     }
   }
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
+  }
+  // Constructor modified by the collaborator
+  public Date(int tag, int monat, int jahr) {
+    this.day = tag;
+    this.month = monat;
+    this.year = jahr;
 }
